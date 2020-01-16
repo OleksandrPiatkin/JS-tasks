578805b462878e3e2bceb49af1b1b2c0dc537884
Alex <Oleksandr.Piatkin@gmx.com>
2020-01-16 16:06:55 +0100

    array tab 1.0

--- HometaskJS_1/task1.js
+++ HometaskJS_1/task1.js
@@ -1,4 +1,29 @@
-let userNumber = prompt("give me number");
+// let userNumber = prompt("give me number");
+
+document.querySelector("#tabBtn").addEventListener("click", createTab);
+    
+// let userNum = 
+function createTab() {
+   let userNumber = document.querySelector("#tabInp").value;
+   let userArr = arrayTable(userNumber);
+   let table = document.querySelector("#table");
+   
+
+   for (let i = 0; i < userArr.length; i++) {
+       let tr = document.createElement('tr');
+
+       for (let j = 0; j < userArr[i].length; j++) {
+            let td = document.createElement('td');
+            td.innerHTML = userArr[i][j];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+   }
+}
+
+let x = createTab();
+console.log(x);
+
 
 function arrayTable( num ) {
     const row = Math.ceil(Math.sqrt(num));
@@ -8,61 +33,50 @@ function arrayTable( num ) {
 
     //creating an array 
 
-    for(var i = 0; i < row; i++) {
+    for(let i = 0; i < row; i++) {
         newArray[i] = new Array(col);
 
-        for(var j = 0; j < col; j++) {
+        for(let j = 0; j < col; j++) {
             newArray[i][j] = "";
         }
     } 
-    
-    
+
         let k, z, y;
         for (k = 0, z = 1, y = 2; k < Math.ceil(row/2), start <= num; k++, z++, y++) {
 
             //top loop 
-                for (let i = k; i < z; i++) {
+            for (let i = k; i < z; i++) {
 
-                    for (let j = k; j < col - k && start <= num; j++) {
-                        newArray[i][j] = start++;
-                    }
+                for (let j = k; j < col - k && start <= num; j++) {
+                    newArray[i][j] = start++;
                 }
+            }
             
             //right loop
-            if (start <= num) {
-                for (let j = col - z; j < col -k; j++) {
+            for (let j = col - z; j < col -k; j++) {
 
-                    for (let i = z; i < col -k && start <= num; i++) {
-                    newArray[i][j] = start++;
-                    }
+                for (let i = z; i < col -k && start <= num; i++) {
+                newArray[i][j] = start++;
                 }
             }
 
             //bottom loop
-            if (start <= num) {
-                for (let i = col - z; i > col - y; i--) {
+            for (let i = col - z; i > col - y; i--) {
 
-                    for (let j = col - y; j >= k && start <= num;j--) {
-                        newArray[i][j] = start++;
-                    }
+                for (let j = col - y; j >= k && start <= num;j--) {
+                    newArray[i][j] = start++;
                 }
             }
 
             //left loop
-            if (start <= num) {
-                for (let j = k; j < z; j++) {
+            for (let j = k; j < z; j++) {
 
-                    for (let i = col - y; i > k && start <= num; i--) {
-                    newArray[i][j] = start++;
-                    }
-                } 
-            }
-  } 
-
+            for (let i = col - y; i > k && start <= num; i--) {
+                newArray[i][j] = start++;
+                }
+            } 
+        } 
   return newArray;
-
 }
 
 
-var x = arrayTable(userNumber);
-console.log(x);
