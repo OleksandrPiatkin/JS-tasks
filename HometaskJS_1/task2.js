6e92de4acaf15949a8305e2d022744d896249ca9
Alex <Oleksandr.Piatkin@gmx.com>
2020-02-03 17:13:59 +0100

    tast 1.2 v1.0

--- HometaskJS_1/task2.js
+++ HometaskJS_1/task2.js
@@ -6,21 +6,27 @@ const text = `Городничий. Я пригласил вас, господа
 Артемий Филиппович. Вот не было заботы, так подай!
 Лука Лукич. Господи Боже! еще и с секретным предписаньем!`;
 
+const actorNames = [ 'Городничий', 'Аммос Федорович', 'Лука Лукич' ];
 const sepText= text.split('\n');
-console.log('separated text', sepText);
- 
 
-let actorText = "";
+let test  = setRoleText2(actorNames);
+console.log('testing tne 2d funct\n', test);
+
+function setRoleText2(names) { 
+    let outPut = '';
+    names.forEach((item) => {
+        outPut = `${outPut}${item}\n${setRoleText(item)}\n`;
+    });
+    return outPut;
+}
 
 function setRoleText(name) {
 
+    let actorText = '';
     sepText.forEach(( item, index ) => {
         if (item.indexOf(name) >= 0) { 
-            actorText = actorText + `${index + 1})` + item.slice(name.length + 2) + '\n';
+            actorText = `${actorText}${index + 1})${item.slice(name.length + 1)}\n`;
          }
       });
     return actorText;
 }
-
-let xx = setRoleText('Городничий');
-console.log(xx);
