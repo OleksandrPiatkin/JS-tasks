8db7f134e88d5d5f3d427354552cc96faf0e783e
Alex <Oleksandr.Piatkin@gmx.com>
2020-03-16 16:34:41 +0100

    task 3 v1.0

--- HometaskJS_3/script.js
+++ HometaskJS_3/script.js
@@ -1,33 +1,35 @@
-const c = document.getElementById("myCanvas");
+const c = document.getElementById("canvas");
+const f = document.getElementById("field");
 
+let height = 500;
+let width = 700;
+
+let arr = [];
 
 class BasicChar {
     constructor() {
         this.x = 0;
         this.y = 0;
-        this.radius = Math.random() * (100 - 15) + 15;
-        this.color = `rgb(${randNum()}, ${randNum()}, ${randNum()})`;
-        this.speedX = Math.random() * 5;
-        this.speedY = 5 - this.speedX;
+        this.radius = Math.random() * (50 - 15) + 15;
+        this.color = `rgb(${this.randNum()}, ${this.randNum()}, ${this.randNum()})`;
+        this.speedX = Math.random();
+        this.speedY = 1 - this.speedX;
         this.flagX = false;
         this.flagY = false;
-        this.height = 600;
-        this.wids = 900;
     }
+    randNum = () => Math.round(Math.random() * 255)
 }
-
-let randNum = function () { return Math.random() * 255; };
 
 class Circle extends BasicChar {
 
     constructor() {
         super();
-        this.sq = Math.PI * this.radius ^ 2;
+        this.sq = Math.round(Math.PI * this.radius * this.radius);
+        this.name = "Circle";
     }
 
     animate() {
         let ctx = c.getContext("2d");
-        ctx.clearRect(0, 0, this.wids, this.height);
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, true);
         ctx.fillStyle = this.color;
@@ -36,36 +38,95 @@ class Circle extends BasicChar {
         this.x += this.speedX;
         this.y += this.speedY;
 
-        console.log("radius log", this.speedY);
-        console.log('Y', this.y);
-
         if ((this.x <= this.radius && this.flagX === true) ||
-            (this.x >= this.wids - this.radius)) {
+            (this.x >= width - this.radius)) {
             this.speedX = -this.speedX;
             this.flagX = true;
         }
         if ((this.y <= this.radius && this.flagY === true) ||
-            (this.y >= this.height - this.radius)) {
+            (this.y >= height - this.radius)) {
             this.speedY = -this.speedY;
             this.flagY = true;
-
-            console.log('flagY 1', this.flagY);
         }
     }
 }
 
-function create() {
-    let circle = new Circle();
-    
-    setInterval(() => {
-        circle.animate();
-    }, 25);
+class Box extends BasicChar {
+
+    constructor() {
+        super();
+        this.side = this.radius * 2;
+        this.sq = Math.round(this.side * this.side);
+        this.name = "Box";
+    }
+
+    animate() {
+        let ctx = c.getContext("2d");
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.side, this.side);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if ((this.x <= 0 && this.flagX === true) ||
+            (this.x >= width - this.side)) {
+            this.speedX = -this.speedX;
+            this.flagX = true;
+        }
+        if ((this.y <= 0 && this.flagY === true) ||
+            (this.y >= height - this.side)) {
+            this.speedY = -this.speedY;
+            this.flagY = true;
+        }
+    }
 }
 
-setInterval(() => {
-    create();
+function createCircle() {
+    let circle = new Circle();
+    arr.push(circle);
+}
+function createBox() {
+    let box = new Box();
+    arr.push(box);
+}
+
+let i = 0;
+let createInterval = setInterval(() => {
+    if (i % 2 == 0) {
+        createCircle();
+        i++;
+    } else {
+        createBox();
+        i++;
+    }
+    if (i > 19) {
+        clearInterval(createInterval);
+    }
+    createTableItems();
 }, 5000);
 
+let animationInterval = setInterval(() => {
+    let ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, width, height);
+    arr.forEach(circle => circle.animate());
+}, 5);
 
+function createTableItems() {
+    let table = document.getElementById("table");
+    let trFigure = document.createElement("tr");
+    table.appendChild(trFigure);
 
+    let trNum = document.createElement("td");
+    trNum.innerHTML = i;
+    trFigure.appendChild(trNum);
 
+    let trType = document.createElement("td");
+    trType.innerHTML = arr[i - 1].name;
+    trFigure.appendChild(trType);
+
+    let trSquare = document.createElement("td");
+    trSquare.innerHTML = arr[i - 1].sq;
+    trFigure.appendChild(trSquare);
+}
